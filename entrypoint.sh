99a0320fd21a39e55083aff329da4e7be929c9d0
Jussi Raunio <jussi.raunio@gmail.com>
2018-08-13 11:26:09 +0300

    Fix argument expansion position in entrypoint.sh.

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,4 +7,4 @@ socat tcp-listen:5554,bind=$ip,fork tcp:127.0.0.1:5554 &
 socat tcp-listen:5555,bind=$ip,fork tcp:127.0.0.1:5555 &
 
 echo "no" | /usr/local/android-sdk/tools/android create avd -f -n test -t ${ANDROID_PLATFORM} --abi default/${ANDROID_ARCH}
-echo "no" | /usr/local/android-sdk/tools/emulator64-${ANDROID_ARCH} -avd test -noaudio -no-window -gpu off -verbose -qemu -usbdevice tablet -vnc "$@" :0
+echo "no" | /usr/local/android-sdk/tools/emulator64-${ANDROID_ARCH} -avd test -noaudio -no-window -gpu off -verbose -qemu -usbdevice tablet -vnc :0 "$@"
